967350abbee71c202fa140f61887a81a5bfa2f42
Ézsely Szovát <ezselyszovat@gmail.com>
2021-11-14 19:51:35 +0100

    initializr

---
+++ job-source/src/main/java/interview/jobsource/JobSourceApplication.java
@@ -0,0 +1,13 @@
+package interview.jobsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSourceApplication.class, args);
+	}
+
+}
